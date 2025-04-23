56c637ea12dd5d1c5ece0f3148b4d45651c9cc47
retr0rcde <130926366+retr0rcde@users.noreply.github.com>
2025-04-23 10:03:00 +0000

    guardar

--- exercises/01.3-Print-the-last-one/app.py
+++ exercises/01.3-Print-the-last-one/app.py
@@ -1,16 +1,16 @@
-# import the random package here "import random"
 import random
 
 def generate_random_list():
     aux_list = []
-    randonlength = random.randint(1, 100)
+    randomlength = random.randint(1, 100)  # Tamaño aleatorio para la lista
 
-    for i in range(randonlength):
-        aux_list.append(randonlength)
-        i += i
+    for i in range(randomlength):
+        aux_list.append(random.randint(1, 100))  # Agregar un número aleatorio entre 1 y 100
     return aux_list
 
 my_stupid_list = generate_random_list()
 
-# Write your code below this comment, good luck!
+# Obtener el último elemento de la lista
+the_last_one = my_stupid_list[-1]
+print(the_last_one)
 
--- exercises/01.5-Add-item-to-list/app.py
+++ exercises/01.5-Add-item-to-list/app.py
@@ -1,5 +1,13 @@
 # Remember to import random function here
+import random
 
 my_list = [4, 5, 734, 43, 45]
 
 # The magic goes below
+for item in range(10):
+    my_list.append(random.randint(1, 100))
+
+
+print(my_list)
+
+
